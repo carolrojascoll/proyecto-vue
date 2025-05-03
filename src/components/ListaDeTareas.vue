2519ce9ae71c2a847a729deb35ec94c81afd5026
Carolina Rojas <carolirojas73@gmail.com>
2025-05-03 16:25:38 -0300

    contador, lista de tareas y Barra Lateral

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h2>Lista de Treas</h2>
+  <input type="text" v-model="nuevaTarea" @keyup.enter="agregarTarea">
+  <ul>
+    <li v-for="(tarea, index) in tareas" :key="index">
+      {{ tarea }}
+      <button @click="eliminarTarea(index)">Eliminar</button>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const agregarTarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+  }
+const eliminarTarea = (index) => {
+    tareas.value.splice(index, 1);
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+  <Sidebar></Sidebar>
+  <div class="contenido">
+    <router-view></router-view>
+  </div>
+
+</div>
+</template>
+
+<script setup>
+import Sidebar from '../components/Sidebar.vue';
+</script>
+
+<style>
+.container {
+  display: flex;
+}
+.contenido {
+  flex: 1;
+  padding: 20px;
+}
+</style>
