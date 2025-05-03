dfdf783494dbe9e117351070109541ba0d93ae73
Carolina Rojas <carolirojas73@gmail.com>
2025-05-03 15:38:38 -0300

    mi primer proyecto con vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<Contador> </Contador>
+</template>
+
+<script setup>
+import Contador from'./components/Contador.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+<template>
+<h1>Hola Mundo</h1>
+<p>{{contador}}</p>
+<button @click="incrementar">Incrementar</button>
+<button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+};
+const decrementar = () => {
+  contador.value--;
+};
+</script>
+
+<script>
+export default {
+  name: "MiContador",
+};
+</script>
+
+<style scoped>
+p{
+  color: red;
+}
+</style>
